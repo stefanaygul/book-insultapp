fe25e562119d0c56f45da974edfc3bbf66e169ae
Stefan <stefan_aygul521@hotmail.com>
2020-10-21 12:18:09 +0300

    added 2 java file

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -1,22 +1,20 @@
 package org.openshift;
-import
-import
-import
-import
-import
-java.util.HashMap;
-javax.enterprise.context.RequestScoped;
-javax.ws.rs.GET;
-javax.ws.rs.Path;
-javax.ws.rs.Produces;
+
+import java.util.HashMap;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 @RequestScoped
 @Path("/insult")
 public class InsultResource {
-@GET()
-@Produces("application/json")
-public HashMap<String,String> getInsult() {
-HashMap<String, String> theInsult = new HashMap<String, String>();
-theInsult.put("insult", new InsultGenerator().generateInsult());
-return theInsult;
- }
+	
+	@GET()
+	@Produces("application/json")
+	public HashMap<String,String> getInsult() {
+		HashMap<String, String> theInsult = new HashMap<String, String>();
+		theInsult.put("insult", new InsultGenerator().generateInsult());
+		return theInsult;
+	}
 }
